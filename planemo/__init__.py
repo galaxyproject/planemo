20a86807cb7ea87db2dbc0197ae08a40df3ab2bc
John Chilton <jmchilton@gmail.com>
2014-10-07 22:29:41 -0400

    Trim generated .travis.yml by fetching install script dynmically.
    Not sure of the wisdom of this - one one hand we can fix things in planemo as time goes on and that is good and may prove needed since we are targeting galaxy-central. On the other hand this is less tweakable and may in fact be more likely to break - hard to say.
    
    Example of workin generated .travis.yml file here https://github.com/jmchilton/bwa-mem/commit/c7a5133f1d3574079c40f8e2c20b9202fb8f72af.

--- planemo/__init__.py
+++ planemo/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __version__ = '0.0.1'
+
+
+PROJECT_NAME = "planemo"
+PROJECT_USERAME = "jmchilton"
+RAW_CONTENT_URL = "https://raw.github.com/%s/%s/master/" % (
+    PROJECT_USERAME, PROJECT_NAME
+)
