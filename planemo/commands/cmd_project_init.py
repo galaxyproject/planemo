68d287ff22df80eb3cc86464baddcdbba0d23fad
John Chilton <jmchilton@gmail.com>
2014-12-04 21:33:45 -0500

    Refactor code for downloading and streaming tars out of project_init.
    I think this will prove useful for shed_diff stuff - too many low-level details in project_init anyway.

--- planemo/commands/cmd_project_init.py
+++ planemo/commands/cmd_project_init.py
@@ -6,13 +6,12 @@ import click
 
 from planemo.cli import pass_context
 from planemo import options
-from planemo.io import warn, shell
-from galaxy.tools.deps.commands import which
+from planemo.io import warn, untar_to
 
 SOURCE_HOST = "https://codeload.github.com"
 DOWNLOAD_URL = "%s/galaxyproject/planemo/tar.gz/master" % SOURCE_HOST
 UNTAR_FILTER = "--strip-components=3 --wildcards --no-anchored '%s/**'"
-UNTAR_CMD = "tar -C %s -zxvf - " + UNTAR_FILTER
+UNTAR_ARGS = "-C %s -zxvf - " + UNTAR_FILTER
 
 
 @click.command("project_init")
@@ -32,10 +31,5 @@ def cli(ctx, path, template=None, **kwds):
     if template is None:
         return
 
-    if which("wget"):
-        download_cmd = "wget -O - %s"
-    else:
-        download_cmd = "curl %s"
-    download_cmd = download_cmd % DOWNLOAD_URL
-    untar_cmd = UNTAR_CMD % (path, template)
-    shell("%s | %s" % (download_cmd, untar_cmd))
+    untar_args = UNTAR_ARGS % (path, template)
+    untar_to(DOWNLOAD_URL, path, untar_args)
