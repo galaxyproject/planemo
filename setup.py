191791e3434e3c51fa0372cb027b5a9fbc58b403
John Chilton <jmchilton@gmail.com>
2014-12-16 23:51:19 -0500

    Fix version, drop Python 3 for now :(.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='planemo',
-    version='0.0.1',
+    version='0.1.0',
     description='Command-line utilities to assist in building tools for the Galaxy project (http://galaxyproject.org/).',
     long_description=readme + '\n\n' + history,
     author='Galaxy Project and Community',
@@ -58,8 +58,6 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
     tests_require=test_requirements
