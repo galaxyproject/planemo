9862713266c202cd994dbbc6e8ea9c8f83d2565f
John Chilton <jmchilton@gmail.com>
2015-03-18 23:39:43 -0400

    Test case for repo creation.

--- tests/test_shed.py
+++ tests/test_shed.py
@@ -29,15 +29,21 @@ class ShedTestCase(CliTestCase):
 
     @skip_unless_environ("TEST_TOOL_SHED_API_KEY")
     def test_shed(self):
-        shed_url = os.environ.get("TEST_TOOL_SHED_URL", "http://localhost:9009")
+        shed_url = os.environ.get("TEST_TOOL_SHED_URL",
+                                  "http://localhost:9009")
         shed_api_key = os.environ.get("TEST_TOOL_SHED_API_KEY")
         tsi = toolshed.ToolShedInstance(shed_url, key=shed_api_key)
         owner = username(tsi)
+        name = "planemotestrepo%d" % random.randint(0, 1000000)
         with self._isolate():
             shed_yml_contents = SHED_TEMPLATE.safe_substitute(
                 owner=owner,
-                name="planemotestrepo%d" % random.randint(0, 1000000),
+                name=name,
             )
             open(".shed.yml", "w").write(shed_yml_contents)
-            # init_cmd = ["shed_repo_create", "--shed_key", shed_api_key, "--shed_target", shed_url]
-            # self._check_exit_code(init_cmd)
+            init_cmd = [
+                "shed_repo_create",
+                "--shed_key", shed_api_key,
+                "--shed_target", shed_url
+            ]
+            self._check_exit_code(init_cmd)
