9061d16397b3262e8e9f97f7646c64c4a699ca97
John Chilton <jmchilton@gmail.com>
2015-04-22 21:17:16 -0400

    Run test shed lint on more valid test repositories.

--- tests/test_shed_lint.py
+++ tests/test_shed_lint.py
@@ -7,3 +7,11 @@ class ShedLineTestCase(CliTestCase):
     def test_valid_repo(self):
         with self._isolate_repo("single_tool"):
             self._check_exit_code(["shed_lint"])
+        with self._isolate_repo("multi_repos_nested"):
+            self._check_exit_code(["shed_lint", "--recursive"])
+        with self._isolate_repo("package_1"):
+            self._check_exit_code(["shed_lint"])
+        with self._isolate_repo("suite_1"):
+            self._check_exit_code(["shed_lint"])
+        with self._isolate_repo("workflow_1"):
+            self._check_exit_code(["shed_lint"])
