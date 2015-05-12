galaxyproject/planemo

commit 343902dcf08bf01dfbb95d98f4a7b9c32c30d777

Allow planemo lint to take many paths.
Implements #139.

planemo/options.py
@@ -133,7 +133,7 @@ def required_tool_arg():
     return click.argument('path', metavar="TOOL_PATH", type=arg_type)
 
 
-def optional_tools_arg():
+def optional_tools_arg(multiple=False):
     """ Decorate click method as optionally taking in the path to a tool
     or directory of tools. If no such argument is given the current working
     directory will be treated as a directory of tools.
@@ -145,11 +145,14 @@ def optional_tools_arg():
         readable=True,
         resolve_path=True,
     )
+    name = 'paths' if multiple else 'path'
+    nargs = -1 if multiple else 1
     return click.argument(
-        'path',
+        name,
         metavar="TOOL_PATH",
         default=".",
-        type=arg_type
+        type=arg_type,
+        nargs=nargs,
     )
 
 

tests/test_lint.py
@@ -22,6 +22,15 @@ class LintTestCase(CliTestCase):
             lint_cmd = ["lint", fail_tool]
             self._check_exit_code(lint_cmd, exit_code=1)
 
+    def test_lint_multiple(self):
+        names = ["fail_citation.xml", "fail_order.xml"]
+        paths = list(map(lambda p: os.path.join(TEST_TOOLS_DIR, p), names))
+        self._check_exit_code(["lint"] + paths, exit_code=1)
+        self._check_exit_code(
+            ["lint", "--skip", "citations,xml_order"] + paths,
+            exit_code=0
+        )
+
     def test_skips(self):
         fail_citation = os.path.join(TEST_TOOLS_DIR, "fail_citation.xml")
         lint_cmd = ["lint", fail_citation]

planemo/commands/cmd_lint.py
@@ -10,21 +10,21 @@ from planemo.tool_lint import lint_tools_on_path
 
 
 @click.command('lint')
-@options.optional_tools_arg()
+@options.optional_tools_arg(-1)
 @options.report_level_option()
 @options.fail_level_option()
 @options.skip_option()
 @options.lint_xsd_option()
 @options.recursive_option()
 @pass_context
-def cli(ctx, path, **kwds):
+def cli(ctx, paths, **kwds):
     """Check specified tool(s) for common errors and adherence to best
     practices.
     """
     lint_args = build_lint_args(ctx, **kwds)
     exit = lint_tools_on_path(
         ctx,
-        path,
+        paths,
         lint_args,
         recursive=kwds["recursive"]
     )

planemo/tool_lint.py
@@ -13,18 +13,19 @@ LINTING_TOOL_MESSAGE = "Linting tool %s"
 SHED_FILES = ["tool_dependencies.xml", "repository_dependencies.xml"]
 
 
-def lint_tools_on_path(ctx, path, lint_args, **kwds):
+def lint_tools_on_path(ctx, paths, lint_args, **kwds):
     assert_tools = kwds.get("assert_tools", True)
     recursive = kwds.get("recursive", False)
     exit = 0
     valid_tools = 0
-    for (tool_path, tool_xml) in yield_tool_xmls(ctx, path, recursive):
-        info("Linting tool %s" % tool_path)
-        if not lint_xml(tool_xml, **lint_args):
-            error("Failed linting")
-            exit = 1
-        else:
-            valid_tools += 1
+    for path in paths:
+        for (tool_path, tool_xml) in yield_tool_xmls(ctx, path, recursive):
+            info("Linting tool %s" % tool_path)
+            if not lint_xml(tool_xml, **lint_args):
+                error("Failed linting")
+                exit = 1
+            else:
+                valid_tools += 1
     if exit == 0 and valid_tools == 0 and assert_tools:
         exit = 2
     return exit

commit 1e3668afacf2f32287ae22b85999257c822239fa

Fix default behavior for ``planemo lint`` to use current directory.
Add tests to verify the default behavior when explicit paths are not supplied. Broken with 343902d.

Thanks to @nekrut for the bug report.

planemo/options.py
@@ -1,5 +1,8 @@
 """ Click definitions for various shared options and arguments.
 """
+
+import os
+
 import click
 from galaxy.tools.deps import docker_util
 
@@ -142,6 +145,13 @@ def required_tool_arg():
     return click.argument('path', metavar="TOOL_PATH", type=arg_type)
 
 
+def _optional_tools_default(ctx, param, value):
+    if param.name == "paths" and len(value) == 0:
+        return [os.path.abspath(os.getcwd())]
+    else:
+        return value
+
+
 def optional_tools_arg(multiple=False):
     """ Decorate click method as optionally taking in the path to a tool
     or directory of tools. If no such argument is given the current working
@@ -159,9 +169,9 @@ def optional_tools_arg(multiple=False):
     return click.argument(
         name,
         metavar="TOOL_PATH",
-        default=".",
         type=arg_type,
         nargs=nargs,
+        callback=_optional_tools_default,
     )
 
 

tests/data/repos/single_tool/cat.xml
@@ -4,13 +4,13 @@
         cat $input1 #for $q in $queries# ${q.input2} #end for# > $out_file1
     </command>
     <inputs>
-        <param name="input1" type="data" label="Concatenate Dataset"/>
+        <param name="input1" type="data" label="Concatenate Dataset" format="data" />
         <repeat name="queries" title="Dataset">
-            <param name="input2" type="data" label="Select" />
+            <param name="input2" type="data" label="Select" format="data" />
         </repeat>
     </inputs>
     <outputs>
-        <data name="out_file1" format="input" metadata_source="input1"/>
+        <data name="out_file1" format_source="input1" metadata_source="input1"/>
     </outputs>
     <tests>
       <test>
@@ -19,5 +19,6 @@
       </test>
     </tests>
     <help>
+        Concatenate datasets.
     </help>
 </tool>

tests/test_lint.py
@@ -22,6 +22,12 @@ class LintTestCase(CliTestCase):
             lint_cmd = ["lint", fail_tool]
             self._check_exit_code(lint_cmd, exit_code=1)
 
+    def test_lint_default(self):
+        with self._isolate_repo("single_tool"):
+            self._check_exit_code(["lint", "--skip", "citations"])
+        with self._isolate_repo("single_tool"):
+            self._check_exit_code(["lint"], exit_code=1)
+
     def test_lint_multiple(self):
         names = ["fail_citation.xml", "fail_order.xml"]
         paths = list(map(lambda p: os.path.join(TEST_TOOLS_DIR, p), names))