galaxyproject/planemo

commit 48e0ca7fa08f7244f6e95982f571a93c54496eea

attempt at fixing silent failure
when shed_uploading but repo does not exist on target

planemo/shed/__init__.py
@@ -172,13 +172,14 @@ def upload_repository(ctx, realized_repository, **kwds):
         repo_id = realized_repository.create(ctx, tsi)
     # failing to create the repo, give up
     if repo_id is None:
+        error("Repository [%s] does not exist in the targeted Tool Shed." % name)
         return -1
 
     if kwds["check_diff"]:
         is_diff = diff_repo(ctx, realized_repository, **kwds)
         if not is_diff:
             name = realized_repository.name
-            info("Repositry [%s] not different, skipping upload." % name)
+            info("Repository [%s] not different, skipping upload." % name)
             return 0
 
     # TODO: support updating repo information if it changes in the config file

commit 425852aea07fdd44fea9f3da24853ea6c288f4cd

fix to #203
the referenced variable was not intialized
broken in  https://github.com/galaxyproject/planemo/commit/48e0ca7fa08f7244f6e95982f571a93c54496eea

planemo/shed/__init__.py
@@ -172,6 +172,7 @@ def upload_repository(ctx, realized_repository, **kwds):
         repo_id = realized_repository.create(ctx, tsi)
     # failing to create the repo, give up
     if repo_id is None:
+        name = realized_repository.name
         error("Repository [%s] does not exist in the targeted Tool Shed." % name)
         return -1
 