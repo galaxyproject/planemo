238c20e377ea6b29c152efb96ada356a3172415d
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-08-28 00:15:55 +0200

    add new category

--- scripts/categories.py
+++ scripts/categories.py
@@ -5,7 +5,6 @@ import requests
 categories = requests.get("https://testtoolshed.g2.bx.psu.edu/api/categories").json()
 print("CURRENT_CATEGORIES = [")
 for c in map(lambda c: c["name"], categories):
-     if c["
      print('    "%s",' % c)
 print("]")
  
