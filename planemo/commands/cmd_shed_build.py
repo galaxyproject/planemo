61c4ef93e88b5d4a100498ccda9805735314608f
Kyle Ellrott <kellrott@gmail.com>
2015-09-11 10:08:12 -0700

    Removing print statement.

--- planemo/commands/cmd_shed_build.py
+++ planemo/commands/cmd_shed_build.py
@@ -18,11 +18,10 @@ def cli(ctx, path, **kwds):
     """
 
     def build(realized_repository):
-        print realized_repository.real_path, realized_repository.name
         tarpath = shed.build_tarball(realized_repository.real_path)
         outpath = realized_repository.real_path + ".tar.gz"
         shutil.move(tarpath, outpath)
-        print "Created: %s" % (outpath)
+        print("Created: %s" % (outpath))
         return 0
 
     exit_code = shed.for_each_repository(ctx, build, [path], **kwds)
