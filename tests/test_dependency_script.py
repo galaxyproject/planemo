7073505d0524c01f2777dfafd84a5ba3350c7528
peterjc <p.j.a.cock@googlemail.com>
2015-09-30 16:14:04 +0100

    Try to test planemo dependency_script --download_cache

--- tests/test_dependency_script.py
+++ tests/test_dependency_script.py
@@ -1,10 +1,28 @@
 import os
 import glob
+import tempfile
 
 from .test_utils import (
     CliTestCase,
     TEST_REPOS_DIR,
 )
+
+
+# This is a bit of a hack, but having failed to get the test
+# script to see $INSTALL_DIR and $DOWNLOAD_CACHE set via
+# .travis.yml this is an alternative.
+temp_dir = tempfile.mkdtemp()
+for env_var, default in [
+        ("INSTALL_DIR", os.path.join(temp_dir, "install_dir")),
+        ("DOWNLOAD_CACHE", os.path.join(temp_dir, "download_cache")),
+        ]:
+    if env_var not in os.environ:
+        os.environ[env_var] = default
+        os.makedirs(default)
+    value = os.environ[env_var]
+    print("Using $%s=%s" % (env_var, value))
+    assert os.path.isdir(value), value
+# TODO - Remove temp_dir
 
 
 class LintTestCase(CliTestCase):
@@ -16,7 +34,9 @@ class LintTestCase(CliTestCase):
         self._check_exit_code(ds_cmd, 1)
 
     def test_samtools_example(self):
+        # Also checking the --download_cache option
         ds_cmd = ["dependency_script",
+                  "--download_cache", os.environ["DOWNLOAD_CACHE"],
                   os.path.join(TEST_REPOS_DIR, "package_1")]
         self._check_exit_code(ds_cmd, 0)
 
