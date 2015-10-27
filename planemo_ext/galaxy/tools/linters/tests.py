0a122a9424074f6fe586e07a8e818a0151de633d
Eric Rasche <rasche.eric@gmail.com>
2015-10-26 20:49:42 -0500

    Correct test outputs for #232

--- planemo_ext/galaxy/tools/linters/tests.py
+++ planemo_ext/galaxy/tools/linters/tests.py
@@ -3,8 +3,13 @@
 # Misspelled so as not be picked up by nosetests.
 def lint_tsts(tool_xml, lint_ctx):
     tests = tool_xml.findall("./tests/test")
-    if not tests:
+
+    is_datasource = tool_xml.getroot().attrib.get('tool_type', '') == 'data_source'
+
+    if not tests and not is_datasource:
         lint_ctx.warn("No tests found, most tools should define test cases.")
+    elif is_datasource:
+        lint_ctx.info("No tests found, that should be OK for data_sources.")
 
     num_valid_tests = 0
     for test in tests:
@@ -24,7 +29,7 @@ def lint_tsts(tool_xml, lint_ctx):
         else:
             num_valid_tests += 1
 
-    if num_valid_tests:
+    if num_valid_tests or is_datasource:
         lint_ctx.valid("%d test(s) found.", num_valid_tests)
     else:
         lint_ctx.warn("No valid test(s) found.")
