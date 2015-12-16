2f86fdad6722f1a329f055121d7c74fe5eae2527
John Chilton <jmchilton@gmail.com>
2015-12-16 20:13:22 +0000

    Toward a "Galaxy" library.
     - Improve script to sync Galaxy and Planemo.
     - Synchornize more of util.
     - Synchornize tool parsing. (requires outstanding Galaxy PRs)
     - Synchronize abstracct toolbox. (requires outstanding Galaxy PRs)
     - Move Python 3 support (requires outstanding Galaxy PRs).

--- planemo_ext/galaxy/util/bunch.py
+++ planemo_ext/galaxy/util/bunch.py
@@ -1,15 +1,15 @@
-
-
-class Bunch(object):
+class Bunch( object ):
     """
     http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/52308
 
-    Often we want to just collect a bunch of stuff together, naming each item
-    of the bunch; a dictionary's OK for that, but a small do-nothing class is
-    even handier, and prettier to use.
+    Often we want to just collect a bunch of stuff together, naming each item of
+    the bunch; a dictionary's OK for that, but a small do-nothing class is even handier, and prettier to use.
     """
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
+
+    def dict(self):
+        return self.__dict__
 
     def get(self, key, default=None):
         return self.__dict__.get(key, default)
@@ -20,14 +20,20 @@ class Bunch(object):
     def items(self):
         return self.__dict__.items()
 
+    def keys(self):
+        return self.__dict__.keys()
+
+    def values(self):
+        return self.__dict__.values()
+
     def __str__(self):
         return '%s' % self.__dict__
 
     def __nonzero__(self):
         return bool(self.__dict__)
 
-    def __bool__(self):
-        return bool(self.__dict__)
-
     def __setitem__(self, k, v):
         self.__dict__.__setitem__(k, v)
+
+    def __contains__(self, item):
+        return item in self.__dict__
--- planemo_ext/galaxy/util/directory_hash.py
+++ planemo_ext/galaxy/util/directory_hash.py
@@ -1,6 +1,6 @@
 
 
-def directory_hash_id(id):
+def directory_hash_id( id ):
     """
 
     >>> directory_hash_id( 100 )
@@ -10,14 +10,14 @@ def directory_hash_id(id):
     >>> directory_hash_id("777777777")
     ['000', '777', '777']
     """
-    s = str(id)
-    l = len(s)
+    s = str( id )
+    l = len( s )
     # Shortcut -- ids 0-999 go under ../000/
     if l < 4:
-        return ["000"]
+        return [ "000" ]
     # Pad with zeros until a multiple of three
-    padded = ((3 - len(s) % 3) * "0") + s
+    padded = ( ( 3 - len( s ) % 3 ) * "0" ) + s
     # Drop the last three digits -- 1000 files per directory
     padded = padded[:-3]
     # Break into chunks of three
-    return [padded[i * 3: (i + 1) * 3] for i in range(len(padded) // 3)]
+    return [ padded[ i * 3 : (i + 1 ) * 3 ] for i in range( len( padded ) // 3 ) ]
