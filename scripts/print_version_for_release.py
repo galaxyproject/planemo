ca5b6e126752927deb14af1a0600bc8a280668db
John Chilton <jmchilton@gmail.com>
2015-12-19 07:51:18 +0000

    More project generification.

--- scripts/print_version_for_release.py
+++ scripts/print_version_for_release.py
@@ -2,10 +2,13 @@ from __future__ import print_function
 from distutils.version import LooseVersion
 import ast
 import re
+import sys
+
+source_dir = sys.argv[1]
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('planemo/__init__.py', 'rb') as f:
+with open('%s/__init__.py' % source_dir, 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
