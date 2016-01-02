bbec00ea32dfd631defa5050943991476521a829
John Chilton <jmchilton@gmail.com>
2016-01-02 12:57:52 +0000

    Fix for setup.py.

--- planemo/__init__.py
+++ planemo/__init__.py
@@ -8,7 +8,7 @@ PROJECT_OWNER = PROJECT_USERAME = "galaxyproject"
 PROJECT_AUTHOR = 'Galaxy Project and Community'
 PROJECT_EMAIL = 'jmchilton@gmail.com'
 
-PROJECT_URL = "https://github.com/%s/%s" % (PROJECT_OWNER, PROJECT_NAME)
+PROJECT_URL = "https://github.com/galaxyproject/planemo"
 RAW_CONTENT_URL = "https://raw.github.com/%s/%s/master/" % (
     PROJECT_USERAME, PROJECT_NAME
 )
