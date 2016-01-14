417f034ad8bfa4d12bcbbab0034cf332e242afb4
John Chilton <jmchilton@gmail.com>
2016-01-14 06:45:14 +0000

    Fixup that conda test script.

--- tests/scripts/conda_test.sh
+++ tests/scripts/conda_test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
+planemo=`which planemo`
 temp_dir=`mktemp -d`
 cd $temp_dir
-planemo conda_init --conda_prefix $temp_dir/conda
+$planemo conda_init --conda_prefix $temp_dir/conda
 export PATH=$temp_dir/conda/bin:$PATH
 which conda
-conda create --name gxtest
+conda create -y --name gxtest numpy bx-python pysam
 source activate gxtest
-conda install -y numpy bx-python pysam
-git clone https://github.com/galaxyproject/galaxy.git
+git clone --depth 1 https://github.com/galaxyproject/galaxy.git
 cd galaxy
 ./scripts/common_startup.sh --skip-venv --dev-wheels
 cd ..
-planemo project_init --template=demo demo
+$planemo project_init --template=demo demo
 cd demo
-planemo test --galaxy_root ../galaxy --skip_venv cat.xml
+$planemo test --galaxy_root ../galaxy --skip_venv cat.xml
