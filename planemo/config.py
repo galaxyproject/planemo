2cf57addde623c609d7545ff4a2e48678720edd7
John Chilton <jmchilton@gmail.com>
2016-04-11 14:28:21 -0400

    Remove unused code in planemo/config.py.

--- planemo/config.py
+++ planemo/config.py
@@ -22,31 +22,6 @@ def get_default_callback(default, name=None, resolve_path=False):
     return callback
 
 
-# TODO: delete the following stuff.
-def populate_kwds(ctx, options_hash, kwds):
-    """ Iterate over default option hash and use value from
-    ~/.planemo.yml configuration if set or else the specified
-    default. Populate kwds with this configured default value.
-    """
-    for name, default in options_hash.items():
-        _populate_default_output(ctx, name, kwds, default)
-
-
-def _populate_default_output(ctx, kwd_key, kwds, default):
-    kwd_value = kwds.get(kwd_key, None)
-    if kwd_value is None:
-        global_config = ctx.global_config
-        global_config_key = "default_%s" % kwd_key
-        if global_config_key in global_config:
-            default_value = global_config[global_config_key]
-        else:
-            default_value = default
-
-        if default_value:
-            default_value = os.path.abspath(default_value)
-        kwds[kwd_key] = default_value
-
-
 def _default_option(ctx, kwd_key, kwd_value, default):
     value = kwd_value
 
@@ -80,3 +55,10 @@ def read_global_config(config_path):
 
     with open(config_path) as f:
         return yaml.load(f)
+
+
+__all__ = [
+    "global_config_path",
+    "read_global_config",
+    "get_default_callback",
+]
