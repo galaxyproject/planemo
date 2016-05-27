451803315db4a4d5ed72e59a3f646b423bdde57e
nturaga <nitesh.turaga@gmail.com>
2016-05-27 12:03:03 -0400

    Add bioconda helper functions, 1. Write bioconda recipe 2. clone bioconda repo
    Add command line utility, to initiate bioconda recipe when given package name. "conda_recipe_init"

---
+++ planemo/commands/cmd_conda_recipe_init.py
@@ -0,0 +1,52 @@
+"""Module describing the planemo ``conda_recipe_init`` command."""
+import click
+import os
+
+from planemo.cli import command_function
+from planemo import options
+
+from planemo.conda import clone_bioconda_repo, write_bioconda_recipe
+
+
+@click.option(
+    '-p',
+    '--package_name',
+    type=click.STRING,
+    prompt=True,
+    help=("Give the name of a Bioconductor package "
+          "to create a new conda recipe")
+)
+@click.option(
+    '-c',
+    '--clone/--no-clone',
+    default=False,
+    prompt=True,
+    help=("Clone bioconda repository from github or not?")
+)
+@click.option(
+    '-b',
+    '--bioconda_dir_path',
+    type=click.STRING,
+    prompt=True,
+    help=("Give the path to folder containing bioconda repository")
+)
+@click.option(
+    '-u',
+    '--update/--no-update',
+    default=False,
+    help=("Update an existing bioconda recipe")
+)
+@click.command('conda_recipe_init')
+@command_function
+def cli(ctx, **kwds):
+    """Make a conda recipe, given a package name.
+
+    package_name: motifbreakR
+    bioconda_dir = '/Users/nturaga/Documents/workspace'.
+    """
+    package_name = kwds.get("package_name")
+    clone = kwds.get("clone")
+    update = kwds.get("update")
+    bioconda_dir_path = kwds.get("bioconda_dir_path")
+    write_bioconda_recipe(package_name, clone, update, bioconda_dir_path)
+    return
--- planemo/conda.py
+++ planemo/conda.py
@@ -1,10 +1,16 @@
 """ Planemo specific utilities for dealing with conda, extending Galaxy's
 features with planemo specific idioms.
 """
+
 from __future__ import absolute_import
 
 from galaxy.tools.deps import conda_util
 from planemo.io import shell
+from planemo.io import info
+from planemo import git
+import os
+from planemo.bioconductor_skeleton import write_recipe
+
 
 from galaxy.tools.deps.requirements import parse_requirements_from_xml
 from galaxy.tools.loader_directory import load_tool_elements_from_path
@@ -31,3 +37,37 @@ def collect_conda_targets(path, found_tool_callback=None, conda_context=None):
         requirements, containers = parse_requirements_from_xml(tool_xml)
         conda_targets.extend(conda_util.requirements_to_conda_targets(requirements))
     return conda_targets
+
+
+# Bioconda helper functions
+
+
+def clone_bioconda_repo(path):
+    """Clone bioconda repository in given path."""
+    bioconda_repo = "git@github.com:bioconda/bioconda-recipes.git"
+    git.clone(None, bioconda_repo, path)
+    return "git clone of bioconda repo worked"
+
+
+def write_bioconda_recipe(package_name, clone, update, bioconda_dir_path):
+    """Make a bioconda recipe given the package name."""
+    bioconda_recipe_path = os.path.join(bioconda_dir_path, "biconda-recipes")
+    if clone and (not os.path.exists(bioconda_recipe_path)):
+        clone_bioconda_repo(bioconda_recipe_path)
+        info("bioconda-recipes cloned and writing to %s" % bioconda_dir_path)
+    else:
+        info("bioconda repository not cloned or already exists")
+    # Check if package_name is in recipes
+    presence = any(package_name in r for r, d, f in os.walk(bioconda_recipe_path))
+    print("Is the package present? :", presence)
+    if presence:
+        # click.echo("Package already exisits in bioconda")
+        if update:
+            # click.echo("Package was updated in bioconda-repo")
+            recipe_dir = os.path.join(bioconda_recipe_path, "recipes")
+            write_recipe(package_name, recipe_dir, True)
+    elif not presence:
+        info("Package found in bioconda recipes")
+        recipe_dir = os.path.join(bioconda_recipe_path, "recipes")
+        write_recipe(package_name, recipe_dir, True)
+    return
