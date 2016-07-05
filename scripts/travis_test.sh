13a3ae931a905b361e521bd1eec17b32ae071ee2
peterjc <p.j.a.cock@googlemail.com>
2016-07-05 11:55:50 +0100

    Document link to .travis/setup_custom_dependencies.bash

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,6 +3,6 @@ sudo apt-get install -y python-virtualenv
 virtualenv planemo-venv
 . planemo-venv/bin/activate
 pip install planemo
-planemo travis_before_install
+planemo travis_before_install # runs .travis/setup_custom_dependencies.bash
 . ${TRAVIS_BUILD_DIR}/.travis/env.sh # source environment created by planemo
 planemo test --install_galaxy --no_cache_galaxy ${TRAVIS_BUILD_DIR}
