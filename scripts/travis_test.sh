5f66d45d226a0e960986cc0692f25f886768f9d8
peterjc <p.j.a.cock@googlemail.com>
2016-07-05 11:54:53 +0100

    Use planemo test --no_cache_galaxy on TravisCI

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -4,7 +4,5 @@ virtualenv planemo-venv
 . planemo-venv/bin/activate
 pip install planemo
 planemo travis_before_install
- . ${TRAVIS_BUILD_DIR}/.travis/env.sh # source environment created by planemo
-# TODO: Add --no_cache_galaxy once planemo 0.7.0 is available
-# on PyPI.
-planemo test --install_galaxy ${TRAVIS_BUILD_DIR}
+. ${TRAVIS_BUILD_DIR}/.travis/env.sh # source environment created by planemo
+planemo test --install_galaxy --no_cache_galaxy ${TRAVIS_BUILD_DIR}
