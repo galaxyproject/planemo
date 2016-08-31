ca19910c64298611e351e9ea36104117eee6c3c2
John Chilton <jmchilton@gmail.com>
2016-08-31 16:44:18 -0400

    Allow conda_install to take in --conda_auto_init.

--- planemo/commands/cmd_conda_install.py
+++ planemo/commands/cmd_conda_install.py
@@ -6,16 +6,35 @@ from galaxy.tools.deps import conda_util
 from planemo import options
 from planemo.cli import command_function
 from planemo.conda import build_conda_context, collect_conda_targets
-from planemo.io import coalesce_return_codes
+from planemo.exit_codes import EXIT_CODE_FAILED_DEPENDENCIES, ExitCodeException
+from planemo.io import coalesce_return_codes, error
 
 
 @click.command('conda_install')
 @options.optional_tools_arg()
 @options.conda_target_options()
+@options.conda_auto_init_option()
 @command_function
 def cli(ctx, path, **kwds):
     """Install conda packages for tool requirements."""
     conda_context = build_conda_context(ctx, **kwds)
+    if not conda_context.is_conda_installed():
+        auto_init = kwds.get("conda_auto_init", False)
+        failed = True
+        if auto_init:
+            if conda_context.can_install_conda():
+                if conda_util.install_conda(conda_context):
+                    error("Attempted to install conda and failed.")
+                else:
+                    failed = False
+            else:
+                error("Cannot install conda, failing conda_install.")
+        else:
+            error("Conda not configured - run planemo conda_init' or pass --conda_auto_init to continue.")
+
+        if failed:
+            raise ExitCodeException(EXIT_CODE_FAILED_DEPENDENCIES)
+
     return_codes = []
     for conda_target in collect_conda_targets(path):
         ctx.log("Install conda target %s" % conda_target)
