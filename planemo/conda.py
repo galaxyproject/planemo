3a97d8ce29548520183ec75b87a7b31f79742820
nturaga <nitesh.turaga@gmail.com>
2016-10-12 12:03:13 -0400

    add git

--- planemo/conda.py
+++ planemo/conda.py
@@ -12,6 +12,7 @@ from galaxy.tools.deps import conda_util
 from planemo.io import shell
 from planemo.tools import yield_tool_sources_on_paths
 from planemo.io import info
+from planemo import git
 
 from planemo.bioconda_scripts import bioconductor_skeleton
 
