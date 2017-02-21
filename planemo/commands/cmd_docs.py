a51443749c940c10017b07d419a2fd5261d16685
John Chilton <jmchilton@gmail.com>
2017-02-21 16:20:33 -0500

    Fix up and update planemo docs command.

--- planemo/commands/cmd_docs.py
+++ planemo/commands/cmd_docs.py
@@ -3,11 +3,11 @@ import click
 
 from planemo.cli import command_function
 
-SYNTAX_URL = "http://planemo.readthedocs.org/en/latest/"
+DOCS_URL = "http://planemo.readthedocs.io/en/latest/"
 
 
-@click.command("syntax")
+@click.command("docs")
 @command_function
 def cli(ctx, **kwds):
     """Open Planemo documentation in web browser."""
-    click.launch(SYNTAX_URL)
+    click.launch(DOCS_URL)
