7b056a0513fafc3a5e954174ab0d882c49acc7dc
John Chilton <jmchilton@gmail.com>
2018-01-31 22:51:27 -0500

    Try to avoid unhelpful timeout errors on CI servers.

--- tests/test_cmd_serve.py
+++ tests/test_cmd_serve.py
@@ -98,11 +98,11 @@ class ServeTestCase(CliTestCase):
         t.daemon = True
         t.start()
         time.sleep(10)
-        assert network_util.wait_net_service("127.0.0.1", port)
+        assert network_util.wait_net_service("127.0.0.1", port, timeout=600)
         time.sleep(1)
-        assert network_util.wait_net_service("127.0.0.1", port)
+        assert network_util.wait_net_service("127.0.0.1", port, timeout=600)
         time.sleep(1)
-        assert network_util.wait_net_service("127.0.0.1", port)
+        assert network_util.wait_net_service("127.0.0.1", port, timeout=600)
 
     def _run(self, serve_args=[]):
         test_cmd = [
