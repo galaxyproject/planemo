215bcb5007a11cd156b59b996c89da984d6c9408
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2018-02-23 09:52:24 +0000

    Fix for last py34-quick failing test

--- planemo/reports/xunit_handler.py
+++ planemo/reports/xunit_handler.py
@@ -5,5 +5,4 @@ def handle_report_xunit_kwd(kwds, collected_data):
     if kwds.get('report_xunit', False):
         with open(kwds['report_xunit'], 'w') as handle:
             handle.write(template_data(
-                collected_data, template_name='xunit.tpl')
-                .encode('ascii', 'xmlcharrefreplace'))
+                collected_data, template_name='xunit.tpl'))
