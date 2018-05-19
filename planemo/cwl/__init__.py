e387a76dfd155fbfd8abdf6fbdcff2277632af73
John Chilton <jmchilton@gmail.com>
2018-05-18 21:27:35 -0400

    Add ``--toil`` engine type for CWL development.
    More information on Toil can be found at https://github.com/BD2KGenomics/toil.

--- planemo/cwl/__init__.py
+++ planemo/cwl/__init__.py
@@ -1,9 +1,11 @@
 """Entry point for modules describing abstractions for dealing with CWL artifacts."""
 from .run import run_cwltool
 from .script import to_script
+from .toil import run_toil
 
 
 __all__ = (
     'run_cwltool',
+    'run_toil',
     'to_script',
 )
