dd669f89c072818e3d67d7b6797826269b05d174
John Chilton <jmchilton@gmail.com>
2018-05-20 08:24:59 -0400

    [WIP] Fix external workflow testing.
    Test previous behavior as docker_test_path_paste and try to see if default, direct uploads are fixed in 0.52.0.

--- scripts/run_galaxy_workflow_tests.sh
+++ scripts/run_galaxy_workflow_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-: ${PLANEMO_TARGET:="planemo==0.50.1"}
+: ${PLANEMO_TARGET:="planemo==0.52.0"}
 : ${PLANEMO_OPTIONS:=""}  # e.g. PLANEMO_OPTIONS="--verbose"
 : ${PLANEMO_PROFILE_NAME:="wxflowtest"}
 : ${PLANEMO_SERVE_PORT:="9019"}
 : ${PLANEMO_GALAXY_BRANCH:="master"}
-: ${PLANEMO_TEST_STYLE:="serve_and_test"}  # profile_serve_and_test, serve_and_test, docker_serve_and_test, test, docker_test
+: ${PLANEMO_TEST_STYLE:="serve_and_test"}  # profile_serve_and_test, serve_and_test, docker_serve_and_test, test, docker_test, docker_test_path_paste
 : ${PLANEMO_SERVE_DATABASE_TYPE:="postgres"}  # used if not using Docker with PLANEMO_TEST_STYLE
 : ${PLANEMO_DOCKER_GALAXY_IMAGE:="quay.io/bgruening/galaxy:18.01"}  # used if used Docker with PLANEMO_TEST_STYLE
 
@@ -57,7 +57,6 @@ elif [ "$PLANEMO_TEST_STYLE" = "docker_serve_and_test" ]; then
         --daemon \
         --engine docker_galaxy \
         --docker_galaxy_image "${PLANEMO_DOCKER_GALAXY_IMAGE}" \
-        --docker_extra_volume . \
         --port "$PLANEMO_SERVE_PORT" \
         "$1"
     planemo $PLANEMO_OPTIONS test \
@@ -77,7 +76,16 @@ elif [ "$PLANEMO_TEST_STYLE" = "docker_test" ]; then
     planemo $PLANEMO_OPTIONS test \
         --engine docker_galaxy \
         --docker_galaxy_image "${PLANEMO_DOCKER_GALAXY_IMAGE}" \
+        "$1" > /dev/null
+elif [ "$PLANEMO_TEST_STYLE" = "docker_test_path_paste" ]; then
+    # Same as above but mount the test data and use file:// path pastes when uploading
+    # files (more robust and quick if working with really large files).
+    docker pull "${PLANEMO_DOCKER_GALAXY_IMAGE}"
+    planemo $PLANEMO_OPTIONS test \
+        --engine docker_galaxy \
         --docker_extra_volume . \
+        --paste_test_data_paths \
+        --docker_galaxy_image "${PLANEMO_DOCKER_GALAXY_IMAGE}" \
         "$1" > /dev/null
 else
     echo "Unknown test style ${PLANEMO_TEST_STYLE}"
