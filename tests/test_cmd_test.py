f1ed8a908541697b142752fce0fcf87a84d1ab06
John Chilton <jmchilton@gmail.com>
2018-11-22 11:36:11 +0100

    [WIP] Allow composite inputs and collection output tests for workflows.

--- tests/test_cmd_test.py
+++ tests/test_cmd_test.py
@@ -103,6 +103,29 @@ class CmdTestTestCase(CliTestCase):
             #    raise
 
     @skip_if_environ("PLANEMO_SKIP_GALAXY_TESTS")
+    def test_workflow_test_composite(self):
+        """Test testing a workflow that uses composite inputs."""
+        with self._isolate():
+            test_artifact = os.path.join(TEST_DATA_DIR, "wf6-composite-inputs.gxwf.yml")
+            composite_input_imzml = os.path.join(TEST_DATA_DIR, "composite_input_imzml.xml")
+            test_command = [
+                "--verbose",
+                "test"
+            ]
+            test_command = self.append_profile_argument_if_needed(test_command)
+            test_command += [
+                "--no_dependency_resolution",
+                "--extra_tools", composite_input_imzml,
+                test_artifact,
+            ]
+            # try:
+            self._check_exit_code(test_command, exit_code=0)
+            # except Exception:
+            #    with open(os.path.join(f, "tool_test_output.json"), "r") as o:
+            #        print(o.read())
+            #    raise
+
+    @skip_if_environ("PLANEMO_SKIP_GALAXY_TESTS")
     def test_workflow_test_collection_inputs(self):
         """Test testing a workflow with collection inputs Galaxy."""
         with self._isolate():
