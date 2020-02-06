b5c72a63c8032969869dd2c90c73984d826a2adf
John Chilton <jmchilton@gmail.com>
2020-02-06 12:47:14 -0500

    WIP: implement collection output testing for collections.

--- planemo/test/_check_output.py
+++ planemo/test/_check_output.py
@@ -16,6 +16,22 @@ def check_output(runnable, output_properties, test_properties, **kwds):
     of the way galaxy-tool-util throws exceptions instead of returning individual
     descriptions - but this may be enhanced in the future.
     """
+    if "elements" in test_properties:
+        checker = _check_output_collection
+    else:
+        checker = _check_output_file
+    return checker(runnable, output_properties, test_properties, **kwds)
+
+
+def _check_output_collection(runnable, output_properties, test_properties, **kwds):
+    data_collection = self._get("dataset_collections/%s" % output_collection_id, data={"instance_type": "history"}).json()
+
+    def verify_dataset(element, element_attrib, element_outfile):
+        pass
+
+    verify_collection(output_collection_def, data_collection, verify_dataset)
+
+def _check_output_file(runnable, output_properties, test_properties, **kwds):
     get_filename = _test_filename_getter(runnable)
     path = output_properties["path"]
     with open(path, "rb") as fh:
