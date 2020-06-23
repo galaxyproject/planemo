dc1b335915c4fd5f2144c89d1a76e0c034677607
lorrainealisha75 <lorrainealisha75@gmail.com>
2020-06-23 10:42:11 +0200

    First Draft for autoupdate command

---
+++ planemo/commands/autoupdate/autoupdate.py
@@ -0,0 +1,60 @@
+"""Autoupdate older conda dependencies in the requirements section."""
+from __future__ import absolute_import
+
+import planemo.conda
+
+
+def autoupdate(tool_xml):
+    version_dict, main_req_dict = find_packages_in_requirements(tool_xml)
+    main_req_dict = get_main_requirement_version(tool_xml, main_req_dict)
+    updated_version_dict, main_req_updated_version = get_latest_versions(version_dict, main_req_dict)
+    update_requirements(tool_xml, updated_version_dict, main_req_updated_version)
+
+
+def find_packages_in_requirements(tool_xml):
+    packages = {}
+    main_req_dict = {}
+    main_req = None
+    for element in tool_xml.getroot().findall("requirements"):
+        for requirement in list(element):
+            if requirement.tag == 'requirement' and requirement.attrib.get('type', '') == 'package':
+                if requirement.attrib.get('version', '') == '@TOOL_VERSION@':
+                    main_req = requirement.text
+                packages[requirement.text] = requirement.attrib.get('version', '')
+    main_req_dict['main_req'] = main_req
+    return packages, main_req_dict
+
+
+def get_main_requirement_version(tool_xml, main_req_dict):
+    for element in tool_xml.getroot().findall("macros"):
+        for macro in list(element):
+            if macro.tag == 'token' and macro.attrib.get('name', '') == '@TOOL_VERSION@':
+                main_req_dict['@TOOL_VERSION@'] = macro.text
+                return main_req_dict
+    return None
+
+
+def get_latest_versions(version_dict, main_req_dict):
+
+    for package in version_dict.keys():
+        target = planemo.conda.conda_util.CondaTarget(package)
+        search_results = planemo.conda.best_practice_search(target)
+        version_dict[package] = search_results[0]['version']
+
+        main_req_dict['@TOOL_VERSION@'] = version_dict[main_req_dict['main_req']]
+    return version_dict, main_req_dict
+
+
+def update_requirements(tool_xml, updated_version_dict, main_req_updated_version):
+    for element in tool_xml.getroot().findall("requirements"):
+        for requirement in list(element):
+            if requirement.tag == 'requirement' and requirement.attrib.get('type', '') == 'package':
+                if requirement.text == main_req_updated_version['main_req']:
+                    requirement.set('version', '@TOOL_VERSION@')
+                else:
+                    requirement.set('version', updated_version_dict[requirement.text])
+
+    for element in tool_xml.getroot().findall("macros"):
+        for macro in list(element):
+            if macro.tag == 'token' and macro.attrib.get('name', '') == '@TOOL_VERSION@':
+                macro.text = main_req_updated_version['@TOOL_VERSION@']
---
+++ planemo/commands/autoupdate/cmd_autoupdate.py
@@ -0,0 +1,59 @@
+"""Module describing the planemo ``lint`` command."""
+import click
+
+# from os.path import basename
+
+# from galaxy.tool_util.lint import lint_tool_source
+
+from planemo.exit_codes import (
+    EXIT_CODE_GENERIC_FAILURE,
+    EXIT_CODE_OK
+)
+from planemo.io import (
+    coalesce_return_codes,
+    info
+)
+from planemo.tools import (
+    is_tool_load_error,
+    yield_tool_sources_on_paths
+)
+
+from planemo import options
+from planemo.cli import command_function
+from autoupdate import autoupdate
+
+
+@click.command('autoupdate')
+@options.optional_tools_arg(multiple=True, allow_uris=True)
+@options.report_level_option()
+@options.report_xunit()
+@options.fail_level_option()
+@options.skip_option()
+@options.recursive_option()
+@command_function
+def cli(ctx, paths, **kwds):
+    """Check for common errors and best practices."""
+    assert_tools = kwds.get("assert_tools", True)
+    recursive = kwds.get("recursive", False)
+    exit_codes = []
+    for (tool_path, tool_xml) in yield_tool_sources_on_paths(ctx, paths, recursive):
+        info("Auto-updating tool %s" % tool_path)
+        autoupdate(tool_xml)
+        if handle_tool_load_error(tool_path, tool_xml):
+            exit_codes.append(EXIT_CODE_GENERIC_FAILURE)
+            continue
+        else:
+            exit_codes.append(EXIT_CODE_OK)
+    return coalesce_return_codes(exit_codes, assert_at_least_one=assert_tools)
+    ctx.exit()
+
+
+def handle_tool_load_error(tool_path, tool_xml):
+    """ Return True if tool_xml is tool load error (invalid XML), and
+    print a helpful error message.
+    """
+    is_error = False
+    if is_tool_load_error(tool_xml):
+        info("Could not update %s due to malformed xml." % tool_path)
+        is_error = True
+    return is_error
