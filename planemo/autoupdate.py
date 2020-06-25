81a8a83395b80121456a84ec859f29d73fd19a11
lorrainealisha75 <lorrainealisha75@gmail.com>
2020-06-25 14:27:37 +0200

    Basic functionalityof autoupdate check-in

--- planemo/autoupdate.py
+++ planemo/autoupdate.py
@@ -4,59 +4,40 @@ from __future__ import absolute_import
 import planemo.conda
 
 
-def autoupdate(tool_xml):
+def begin_update(tool_path, tool_xml):
     version_dict, main_req_dict = find_packages_in_requirements(tool_xml)
-    main_req_dict = get_main_requirement_version(tool_xml, main_req_dict)
-    updated_version_dict, main_req_updated_version = get_latest_versions(version_dict, main_req_dict)
-    update_requirements(tool_xml, updated_version_dict, main_req_updated_version)
+    updated_version_dict = get_latest_versions(version_dict)
+    return update_requirements(tool_path, tool_xml, updated_version_dict)
 
 
 def find_packages_in_requirements(tool_xml):
     packages = {}
     main_req_dict = {}
-    main_req = None
-    print("fas")
-    print(tool_xml)
-    for element in tool_xml.getroot().findall("requirements"):
+    for element in tool_xml.xml_tree.findall("requirements"):
         for requirement in list(element):
             if requirement.tag == 'requirement' and requirement.attrib.get('type', '') == 'package':
-                if requirement.attrib.get('version', '') == '@TOOL_VERSION@':
-                    main_req = requirement.text
                 packages[requirement.text] = requirement.attrib.get('version', '')
-    main_req_dict['main_req'] = main_req
     return packages, main_req_dict
 
 
-def get_main_requirement_version(tool_xml, main_req_dict):
-    for element in tool_xml.getroot().findall("macros"):
-        for macro in list(element):
-            if macro.tag == 'token' and macro.attrib.get('name', '') == '@TOOL_VERSION@':
-                main_req_dict['@TOOL_VERSION@'] = macro.text
-                return main_req_dict
-    return None
-
-
-def get_latest_versions(version_dict, main_req_dict):
+def get_latest_versions(version_dict):
 
     for package in version_dict.keys():
         target = planemo.conda.conda_util.CondaTarget(package)
         search_results = planemo.conda.best_practice_search(target)
         version_dict[package] = search_results[0]['version']
-
-        main_req_dict['@TOOL_VERSION@'] = version_dict[main_req_dict['main_req']]
-    return version_dict, main_req_dict
+    return version_dict
 
 
-def update_requirements(tool_xml, updated_version_dict, main_req_updated_version):
-    for element in tool_xml.getroot().findall("requirements"):
+def update_requirements(tool_path, tool_xml, updated_version_dict):
+    for element in tool_xml.xml_tree.findall("requirements"):
         for requirement in list(element):
             if requirement.tag == 'requirement' and requirement.attrib.get('type', '') == 'package':
-                if requirement.text == main_req_updated_version['main_req']:
-                    requirement.set('version', '@TOOL_VERSION@')
-                else:
-                    requirement.set('version', updated_version_dict[requirement.text])
+                requirement.set('version', updated_version_dict[requirement.text])
+    tool_xml.xml_tree.write(tool_path)
+    return tool_xml
 
-    for element in tool_xml.getroot().findall("macros"):
-        for macro in list(element):
-            if macro.tag == 'token' and macro.attrib.get('name', '') == '@TOOL_VERSION@':
-                macro.text = main_req_updated_version['@TOOL_VERSION@']
+
+__all__ = (
+    "begin_update"
+)
