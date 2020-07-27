3708a917cd54668e0fd1a73e59051f359ff5bf39
John Chilton <jmchilton@gmail.com>
2020-07-27 13:21:36 -0400

    Mark engine test as consuming a Galaxy branch.

--- tests/test_test_engines.py
+++ tests/test_test_engines.py
@@ -7,9 +7,11 @@ from planemo.runnable import (
     for_paths,
 )
 from .test_utils import (
+    mark,
     PROJECT_TEMPLATES_DIR,
     skip_if_environ,
     skip_unless_module,
+    target_galaxy_branch,
     test_context,
     TEST_DATA_DIR,
 )
@@ -30,6 +32,7 @@ def test_toil_tests():
 
 
 @skip_if_environ("PLANEMO_SKIP_GALAXY_TESTS")
+@mark.tests_galaxy_branch
 def test_galaxy_wf_tests():
     ctx = test_context()
     random_lines = os.path.join(PROJECT_TEMPLATES_DIR, "demo", "randomlines.xml")
@@ -39,6 +42,8 @@ def test_galaxy_wf_tests():
     kwds = {
         "engine": "galaxy",
         "no_dependency_resolution": True,
+        "paste_test_data_paths": False,
+        "galaxy_branch": target_galaxy_branch(),
         "extra_tools": [random_lines, cat]
     }
     exit_code = t_runnables(
