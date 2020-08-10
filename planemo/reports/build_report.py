d1c6d16001f7ead72d7a512e75a7f0f266008a67
mvdbeek <m.vandenbeek@gmail.com>
2020-08-10 11:12:20 +0200

    Only apply non-default jinja2 options to markdown

--- planemo/reports/build_report.py
+++ planemo/reports/build_report.py
@@ -39,7 +39,7 @@ def template_data(environment, report_type, **kwds):
     """Build an arbitrary templated page.
     """
     template_name = 'report_%s.tpl' % report_type
-    env = Environment(loader=PackageLoader('planemo', 'reports'), keep_trailing_newline=report_type == 'markdown', trim_blocks=True)
+    env = Environment(loader=PackageLoader('planemo', 'reports'), keep_trailing_newline=report_type == 'markdown', trim_blocks= == 'markdown')
     env.filters['strip_control_characters'] = lambda x: strip_control_characters(x) if x else x
     template = env.get_template(template_name)
     return template.render(**environment)
