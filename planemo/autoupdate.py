6536192d71aca0e5cb21abbd45a1af6fff636cd7
Simon Bray <sbray1371@gmail.com>
2020-08-20 17:11:23 +0200

    rewrite code

--- planemo/autoupdate.py
+++ planemo/autoupdate.py
@@ -1,6 +1,7 @@
 """Autoupdate older conda dependencies in the requirements section."""
 from __future__ import absolute_import
 
+import collections
 import re
 import xml.etree.ElementTree as ET
 
@@ -10,152 +11,174 @@ import planemo.conda
 from planemo.io import error, info
 
 
+def find_macros(xml_tree):
+    macros = []
+    for macro_import in xml_tree.iter("import"):
+        macros.append(macro_import.text)
+    if macros:
+        return macros
+    else:
+        return None
+
+
+def get_requirements(xml_tree):
+    requirements = {}
+    main_req = None
+    for requirement in xml_tree.iter("requirement"):
+        if requirement.attrib.get('version') == '@TOOL_VERSION@':
+            main_req = requirement.text
+        else:
+            requirements[requirement.text] = requirement.attrib.get('version')
+    return requirements, main_req
+
+
+def get_tokens(xml_tree):
+    tokens = {}
+    for token in xml_tree.iter("token"):
+        tokens[token.attrib['name']] = token.text
+    return tokens
+
+
+def check_conda(tool_name, tool_version, ctx, **kwds):
+    """
+    Update a dict with current conda versions for tool requirements
+    """
+    conda_context = planemo.conda.build_conda_context(ctx, **kwds)
+    target = planemo.conda.conda_util.CondaTarget(tool_name)
+    search_results = conda_util.best_search_result(target, conda_context=conda_context)
+    if search_results[0]['version'] == tool_version:
+        return None
+    else:
+        return search_results[0]['version']
+
+
+def update_xml(tool_path, xml_tree, tags_to_update):
+    """
+    Write modified XML to tool_path
+    """
+
+    print(tool_path)
+    print(tags_to_update)
+
+    def update_token(xml_text, token_name, token_value):
+        return re.sub('{}>.*<{}'.format(*re.split('>.*<', token)), token, xml_text)
+
+    def update_requirement(xml_text, requirement_name, requirement_value):
+        return
+
+    with open(tool_path, 'r+') as f:
+        xml_text = f.read()
+
+        for tag_to_update in tags_to_update:
+            if tag_to_update['type'] = 'token':
+                xml_text = update_token(xml_text, tag_to_update['key'], tag_to_update['value'])
+            if tag_to_update['type'] = 'requirement':
+                update_requirement(xml_text, tag_to_update['key'], tag_to_update['value'])
+
+
+
+
+    # print(tags_to_update)
+    # with open(tool_path, 'r+') as f:
+    #     xml_text = f.read()
+    #     for token in tags_to_update['tokens']:
+    #         xml_text = re.sub('{}>.*<{}'.format(*re.split('>.*<', token)), token, xml_text)
+
+    #     for requirement in tags_to_update['requirements']:
+    #         xml_text = re.sub('{}version=".*"{}'.format(*re.split('version=".*"', requirement)), requirement, xml_text)
+
+    #     # if '@GALAXY_VERSION@' not in tags_to_update['tokens']:
+    #     if tags_to_update.get('update_tool'):
+    #         # update the version directly in the tool tag
+    #         xml_text = re.sub(r'version="@TOOL_VERSION@\+galaxy.*?"', 'version="@TOOL_VERSION@+galaxy0"', xml_text)
+
+    #     f.seek(0)
+    #     f.truncate()
+    #     f.write(xml_text)
+
 def autoupdate(ctx, tool_path, **kwds):
     """
     Autoupdate an XML file
     """
-    xml_tree = ET.parse(tool_path)
-    requirements = find_requirements(xml_tree)
-    for macro_import in requirements['imports']:
-        # recursively check macros
-        macro_requirements = autoupdate(ctx, '/'.join(tool_path.split('/')[:-1] + [macro_import]), **kwds)  #kwds.get('dry_run'))
-        for requirement in macro_requirements:
-            if requirement not in requirements:
-                requirements[requirement] = macro_requirements[requirement]
-    if not requirements.get('@TOOL_VERSION@'):
-        # if tool_version is not specified, finish without changes
-        error("The @TOOL_VERSION@ token is not specified in {}. This is required for autoupdating.".format(tool_path))
-        return requirements
-    updated_main_req = get_latest_versions(ctx, {requirements.get('main_req'): requirements.get('@TOOL_VERSION@')}, **kwds)
-    if updated_main_req[requirements.get('main_req')] == requirements.get('@TOOL_VERSION@'):
-        # check main_req is up-to-date; if so, finish without changes
+    # create a dict of all files that need editing - wrapper plus macros
+    xml_files = {tool_path: ET.parse(tool_path)}
+    for macro in find_macros(xml_files[tool_path]):
+        macro_path = '/'.join(tool_path.split('/')[:-1] + [macro])
+        xml_files[macro_path] = ET.parse(macro_path)
+
+    # create a dict of requirements
+    requirements = {} #collections.defaultdict(dict)
+    main_req = None
+    for k, v in xml_files.items():
+        r, mr = get_requirements(v)
+        requirements[k] = r
+        if mr:
+            if main_req:
+                error('Multiple requirements use the token @TOOL_VERSION@!')
+            main_req = (mr, k)  #{mr: k} 
+    if not main_req:
+        error('No requirement uses the token @TOOL_VERSION@!')
+
+    # create a dict of tokens
+    tokens = {}
+    need_to_update = False
+    xml_to_update = collections.defaultdict(list)
+    for k, v in xml_files.items():
+        tokens[k] = get_tokens(v)
+        if '@TOOL_VERSION@' in tokens[k]:
+            check_main_req = check_conda(main_req[0], tokens[k]['@TOOL_VERSION@'], ctx, **kwds)
+            if check_main_req:
+                xml_to_update[k].append({'type': 'token', 'key': '@TOOL_VERSION@', 'value': check_main_req})
+                need_to_update = True
+    if not need_to_update:
         info("No updates required or made to {}.".format(tool_path))
-        return requirements
+        return  # end here if no update needed
 
-    if kwds.get('dry_run'):
-        error("Update required to {}! Tool main requirement has version {}, newest conda version is {}".format(
-              tool_path, requirements.get('@TOOL_VERSION@'), updated_main_req[requirements.get('main_req')]))
-        return requirements
+    # check all requirements
+    for k, v in requirements.items():
+        for req in v:
+            req_check = check_conda(req, v[req], ctx, **kwds)
+            if req_check:
+                xml_to_update[k].append({'type': 'requirement', 'tag': req, 'value': req_check})
 
-    # if main_req is not up-to-date, update everything
-    updated_version_dict = get_latest_versions(ctx, requirements.get('other_reqs'), **kwds)
-    update_requirements(tool_path, xml_tree, updated_version_dict, updated_main_req)
-    info("Tool {} updated.".format(tool_path))
-    return requirements
+    # check all tokens
+    for k, v in tokens.items():
+        if '@GALAXY_VERSION' in v:
+            xml_to_update[k].append({'type': 'token', 'key': '@GALAXY_VERSION@', 'value': 0})
 
-
-def find_requirements(xml_tree):
-    """
-    Get all requirements with versions as a dictionary of the form
-    {'main_req': main requirement, 'other_reqs': {req1: version, ... },
-    'imports: ['macros.xml'], '*VERSION@': '...'}
-    """
-    requirements = {'other_reqs': {}, 'imports': []}
-
-    # get tokens
-    for token in xml_tree.iter("token"):
-        if 'VERSION@' in token.attrib.get('name', ''):
-            requirements[token.attrib['name']] = token.text
-    for macro_import in xml_tree.iter("import"):
-        requirements['imports'].append(macro_import.text)
-
-    # get requirements
-    for requirement in xml_tree.iter("requirement"):
-        if requirement.attrib.get('version') == '@TOOL_VERSION@':
-            requirements['main_req'] = requirement.text
-        else:
-            requirements['other_reqs'][requirement.text] = requirement.attrib.get('version')
-    return requirements
+    # finally, update each file separately
+    for k, v in xml_files.items():
+        update_xml(k, v, xml_to_update[k])
 
 
-def get_latest_versions(ctx, version_dict, **kwds):
-    """
-    Update a dict with current conda versions for tool requirements
-    """
-    for package in version_dict.keys():
-        conda_context = planemo.conda.build_conda_context(ctx, **kwds)
-        target = planemo.conda.conda_util.CondaTarget(package)
-        search_results = conda_util.best_search_result(target, conda_context=conda_context)
-        if search_results[1]:
-            version_dict[package] = search_results[0]['version']
-        else:
-            error('Requirements could not be found!')
-            exit()
-    return version_dict
+    print(tokens)
+    print(requirements)
+    print(xml_to_update)
+        # requirements[k] = r
+        # if mr:
+        #     if main_req:
+        #         error('Multiple tools use the token @TOOL_VERSION@!')
+        #     main_req = {mr: k}  # (mr, k)
+
+    # tokens = get_tokens(xml_files[tool_path]))
+    
+
+    # print(requirements)
+    # print(main_req)
 
 
-def update_requirements(tool_path, xml_tree, updated_version_dict, updated_main_req):
-    """
-    Update requirements to latest conda versions
-    and update version tokens
-    """
-
-    tags_to_update = {'tokens': [], 'requirements': []}
-
-    # get name of token which defines the wrapper version; if just an integer, None
-    versions = xml_tree.getroot().attrib.get('version')#.split('+galaxy')
-    if versions:
-        versions = versions.split('+galaxy')
-        if len(versions) == 1:
-            error('Tool version is not specified as two parts separated by +galaxy as required by autoupdate.')
-            exit()
-        elif versions[0] != '@TOOL_VERSION@':
-            error('Tool version does not contain @TOOL_VERSION@ as required by autoupdate.')
-            exit()
-        else:
-            if versions[1][0] == versions[1][-1] == '@':
-                wrapper_token = versions[1]
-            else:
-                wrapper_token = None
-    else:
-        wrapper_token = None
-    print(wrapper_token)
-
-    for token in xml_tree.iter("token"):
-        if token.attrib.get('name') == '@TOOL_VERSION@':
-            #  check this
-            token.text = list(updated_main_req.values())[0]
-        elif token.attrib.get('name') == wrapper_token:
-            token.text = '0'
-        else:
-            continue
-        tags_to_update['tokens'].append(ET.tostring(token, encoding="unicode").strip())
-
-    if wrapper_token not in [n.attrib.get('name') for n in xml_tree.iter('token')]:
-        tags_to_update['update_tool'] = True
-
-    for requirement in xml_tree.iter("requirement"):
-        if requirement.text not in updated_main_req:
-            requirement.set('version', updated_version_dict[requirement.text])
-        tags_to_update['requirements'].append(ET.tostring(requirement, encoding="unicode").strip())
-    print(tags_to_update)
-    write_to_xml(tool_path, xml_tree, tags_to_update)
-    return xml_tree
 
 
-def write_to_xml(tool_path, xml_tree, tags_to_update):
-    """
-    Write modified XML to tool_path
-    """
-    print(tags_to_update)
-    with open(tool_path, 'r+') as f:
-        xml_text = f.read()
-        for token in tags_to_update['tokens']:
-            xml_text = re.sub('{}>.*<{}'.format(*re.split('>.*<', token)), token, xml_text)
-
-        for requirement in tags_to_update['requirements']:
-            xml_text = re.sub('{}version=".*"{}'.format(*re.split('version=".*"', requirement)), requirement, xml_text)
-
-        # if '@GALAXY_VERSION@' not in tags_to_update['tokens']:
-        if tags_to_update.get('update_tool'):
-            # update the version directly in the tool tag
-            xml_text = re.sub(r'version="@TOOL_VERSION@\+galaxy.*?"', 'version="@TOOL_VERSION@+galaxy0"', xml_text)
-
-        f.seek(0)
-        f.truncate()
-        f.write(xml_text)
 
 
-__all__ = (
-    "autoupdate"
-)
+
+
+
+# def get_tokens():
+#     return ...
+
+# def conda_check():
+#     return ...
+
+# def update():
+#     return
