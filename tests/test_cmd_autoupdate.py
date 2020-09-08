f27e28298c24ba6e0d66d97f2fcd788cf52d6830
Simon Bray <sbray1371@gmail.com>
2020-09-08 13:58:32 +0200

    another attempt at fixing the test

--- tests/test_cmd_autoupdate.py
+++ tests/test_cmd_autoupdate.py
@@ -2,6 +2,8 @@
 import os
 from shutil import copyfile
 
+from planemo import cli
+
 from .test_utils import (
     CliTestCase,
     TEST_DATA_DIR
@@ -14,26 +16,26 @@ class CmdAutoupdateTestCase(CliTestCase):
     def test_autoupdate_dry_run(self):
         """Test autoupdate command with dry run flag."""
         with self._isolate():
-            training_init_command = [
+            autoupdate_command = [
                 "autoupdate",
                 "{}/autoupdate_test.xml".format(TEST_DATA_DIR),
                 "--conda_channels", "bioconda",
                 "--dry-run"
             ]
-            self._check_exit_code(training_init_command, exit_code=0)
+            self._check_exit_code(autoupdate_command, exit_code=0)
 
     def test_autoupdate(self):
         """Test autoupdate command."""
         with self._isolate() as f:
             copyfile("{}/autoupdate_test.xml".format(TEST_DATA_DIR), os.path.join(f, "autoupdate_test.xml"))
-            training_init_command = [
+            autoupdate_command = [
                 "autoupdate",
                 os.path.join(f, "autoupdate_test.xml"),
                 "--conda_channels", "bioconda"
             ]
-            self._check_exit_code(training_init_command, exit_code=0)
+            self._runner.invoke(cli.planemo, autoupdate_command)
             with open(os.path.join(f, "autoupdate_test.xml")) as f:
                 updated_tool = f.readlines()
                 assert updated_tool[2].strip() == '<token name="@TOOL_VERSION@">0.7.3</token>'
-                assert updated_tool[3].strip() == '<token name="@GALAXY_VERSION@">0</token>'
+                assert updated_tool[3].strip() == '<token name="@VERSION_SUFFIX@">0</token>'
                 assert updated_tool[7].strip() == '<requirement type="package" version="3.7.1">zeroc-ice</requirement>'
