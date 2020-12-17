9760437a2caf6ccb134b69c208016c75003a46d8
Simon Bray <sbray1371@gmail.com>
2020-12-17 13:50:21 +0100

    get test to actually test something

--- tests/test_cmd_workflow_convert.py
+++ tests/test_cmd_workflow_convert.py
@@ -12,11 +12,11 @@ from .test_utils import (
 
 class CmdWorkflowConvertTestCase(CliTestCase):
     def test_gxwf_to_ga(self):
-        with self._isolate():
+        with self._isolate() as f:
             gx2_wf_path = os.path.join(TEST_DATA_DIR, "wf1.gxwf.yml")
-            init_cmd = ["workflow_convert", gx2_wf_path]
+            init_cmd = ["workflow_convert", gx2_wf_path, "-o", "wf1-test.ga"]
             self._check_exit_code(init_cmd)
-            ga_wf_path = os.path.join(TEST_DATA_DIR, "wf1.gxwf.ga")
+            ga_wf_path = os.path.join(f, "wf1-test.ga")
 
             assert os.path.exists(ga_wf_path)
             with open(ga_wf_path, "r") as stream:
@@ -25,11 +25,11 @@ class CmdWorkflowConvertTestCase(CliTestCase):
             assert wf["steps"]["1"]["tool_id"] == "cat"
 
     def test_ga_to_gxwf(self):
-        with self._isolate():
-            ga_wf_path = os.path.join(TEST_DATA_DIR, "wf1.gxwf.ga")
-            init_cmd = ["workflow_convert", ga_wf_path]
+        with self._isolate() as f:
+            ga_wf_path = os.path.join(TEST_DATA_DIR, "wf1.ga")
+            init_cmd = ["workflow_convert", ga_wf_path, "-o", "wf1-test.yml"]
             self._check_exit_code(init_cmd)
-            gx2_wf_path = os.path.join(TEST_DATA_DIR, "wf1.gxwf.gxwf.yml")
+            gx2_wf_path = os.path.join(f, "wf1-test.yml")
 
             assert os.path.exists(gx2_wf_path)
             with open(gx2_wf_path, "r") as stream:
