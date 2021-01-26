6c0626911e417ef724e4178b3c5a63c9a6ae03e5
mvdbeek <m.vandenbeek@gmail.com>
2021-01-26 14:15:47 +0100

    Drop env var got gh repo fork

--- planemo/github_util.py
+++ planemo/github_util.py
@@ -58,9 +58,8 @@ def clone_fork_branch(ctx, target, path, **kwds):
 def fork(ctx, path, remote_name=DEFAULT_REMOTE_NAME, **kwds):
     """Fork the target repository using ``gh``."""
     gh_path = ensure_gh(ctx, **kwds)
-    gh_env = get_gh_env(ctx, path, **kwds)
-    cmd = [gh_path, "repo", "fork", '--remote-name', remote_name]
-    communicate(cmd, env=gh_env)
+    cmd = [gh_path, "repo", "fork", '--remote', '--remote-name', remote_name]
+    communicate(cmd, cwd=path)
     return remote_name
 
 
