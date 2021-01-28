5a6991c852b4ccab49f82ee31224305b2942cfa0
mvdbeek <m.vandenbeek@gmail.com>
2021-01-28 09:50:39 +0100

    Don't let single invalid tool crash conda target collection

--- planemo/conda.py
+++ planemo/conda.py
@@ -118,11 +118,14 @@ def collect_conda_target_lists_and_tool_paths(ctx, paths, recursive=False, found
     conda_target_lists = set([])
     tool_paths = collections.defaultdict(list)
     for (tool_path, tool_source) in yield_tool_sources_on_paths(ctx, paths, recursive=recursive, yield_load_errors=False):
-        if found_tool_callback:
-            found_tool_callback(tool_path)
-        targets = frozenset(tool_source_conda_targets(tool_source))
-        conda_target_lists.add(targets)
-        tool_paths[targets].append(tool_path)
+        try:
+            if found_tool_callback:
+                found_tool_callback(tool_path)
+            targets = frozenset(tool_source_conda_targets(tool_source))
+            conda_target_lists.add(targets)
+            tool_paths[targets].append(tool_path)
+        except Exception:
+            log.exception(f"Error while collecting list of conda targets for '{tool_path}'")
 
     # Turn them into lists so the order matches before returning...
     conda_target_lists = list(conda_target_lists)
