76513377fe90609003c06e6d01fa8e0c798ae47d
mvdbeek <m.vandenbeek@gmail.com>
2022-05-13 13:43:04 +0200

    Use LocalManagedGalaxyEngine instead of run_tests.sh to run tool tests

--- planemo/engine/test.py
+++ planemo/engine/test.py
@@ -1,14 +1,6 @@
 from planemo.engine import engine_context
-from planemo.galaxy import galaxy_config
-from planemo.galaxy.config import _find_test_data
-from planemo.galaxy.test import (
-    handle_reports_and_summary,
-    run_in_config,
-)
-from planemo.runnable import (
-    for_paths,
-    RunnableType,
-)
+from planemo.galaxy.test import handle_reports_and_summary
+from planemo.runnable import RunnableType
 
 
 def test_runnables(ctx, runnables, original_paths=None, **kwds):
@@ -17,18 +9,8 @@ def test_runnables(ctx, runnables, original_paths=None, **kwds):
     test_engine_testable = {RunnableType.galaxy_tool, RunnableType.galaxy_datamanager, RunnableType.directory}
     enable_test_engines = any(r.type not in test_engine_testable for r in runnables)
     enable_test_engines = enable_test_engines or engine_type != "galaxy"
-    if enable_test_engines:
-        ctx.vlog("Using test engine type %s" % engine_type)
-        with engine_context(ctx, **kwds) as engine:
-            test_data = engine.test(runnables)
-            ctx.vlog("engine.test returning [%s]" % test_data)
-            return_value = handle_reports_and_summary(ctx, test_data.structured_data, kwds=kwds)
-    else:
-        ctx.vlog("Running traditional Galaxy tool tests using run_tests.sh in Galaxy root %s" % engine_type)
-        kwds["for_tests"] = True
-        if kwds.get("update_test_data"):
-            non_copied_runnables = for_paths(original_paths)
-            kwds["test_data_target_dir"] = _find_test_data(non_copied_runnables, **kwds)
-        with galaxy_config(ctx, runnables, **kwds) as config:
-            return_value = run_in_config(ctx, config, **kwds)
-    return return_value
+    ctx.vlog("Using test engine type %s" % engine_type)
+    with engine_context(ctx, **kwds) as engine:
+        test_data = engine.test(runnables)
+        ctx.vlog("engine.test returning [%s]" % test_data)
+        return handle_reports_and_summary(ctx, test_data.structured_data, kwds=kwds)
