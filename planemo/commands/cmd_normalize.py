33d05c401d9414e35a5c7772a99f865d8f22f13b
Cristóbal Gallardo <gallardo@informatik.uni-freiburg.de>
2022-10-06 15:24:17 +0200

    Fix sorting

--- planemo/commands/cmd_normalize.py
+++ planemo/commands/cmd_normalize.py
@@ -7,10 +7,10 @@ from galaxy.tool_util.loader import (
     load_tool,
     raw_tool_xml_tree,
 )
+from lxml.etree import _Element
 
 from planemo import options
 from planemo.cli import command_function
-from lxml.etree import _Element
 
 
 @click.command("normalize")
@@ -85,7 +85,7 @@ def cli(ctx, path, expand_macros=False, **kwds):
     ElementTree.dump(root)
 
 
-def _indent(elem: _Element, level: int=0) -> None:
+def _indent(elem: _Element, level: int = 0) -> None:
     # http://stackoverflow.com/questions/749796/pretty-printing-xml-in-python
     i = "\n" + level * "    "
     if len(elem):
