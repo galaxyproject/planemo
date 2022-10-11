bf0cef7b11901701839d0a4a7e66bd08787601af
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2022-10-11 12:27:49 +0100

    Add type annotations to ``planemo.workflow_lint``

--- planemo/workflow_lint.py
+++ planemo/workflow_lint.py
@@ -3,6 +3,16 @@ import json
 import os
 import re
 from collections import OrderedDict
+from typing import (
+    Any,
+    Dict,
+    Iterable,
+    Iterator,
+    List,
+    Optional,
+    TYPE_CHECKING,
+    Union,
+)
 
 import yaml
 from galaxy.tool_util.lint import LintContext
@@ -26,8 +36,12 @@ from planemo.galaxy.workflows import (
 from planemo.runnable import (
     cases,
     for_path,
+    TestCase,
 )
 from planemo.shed import DOCKSTORE_REGISTRY_CONF
+
+if TYPE_CHECKING:
+    from planemo.cli import PlanemoCliContext
 
 POTENTIAL_WORKFLOW_FILES = re.compile(r"^.*(\.yml|\.yaml|\.ga)$")
 DOCKSTORE_REGISTRY_CONF_VERSION = "1.2"
@@ -39,11 +53,11 @@ class WorkflowLintContext(LintContext):
     training_topic = None
 
 
-def generate_dockstore_yaml(directory):
+def generate_dockstore_yaml(directory: str) -> str:
     workflows = []
     for workflow_path in find_workflow_descriptions(directory):
         test_parameter_path = f"{workflow_path.rsplit('.', 1)[0]}-tests.yml"
-        workflow_entry = {
+        workflow_entry: Dict[str, Any] = {
             # TODO: support CWL
             "subclass": "Galaxy",
             "name": "main",
@@ -52,13 +66,15 @@ def generate_dockstore_yaml(directory):
         if os.path.exists(test_parameter_path):
             workflow_entry["testParameterFiles"] = [f"/{os.path.relpath(test_parameter_path, directory)}"]
         workflows.append(workflow_entry)
-    # Force version to the top of file but serializing rest of config seprately
+    # Force version to the top of file but serializing rest of config separately
     contents = "version: %s\n" % DOCKSTORE_REGISTRY_CONF_VERSION
     contents += yaml.dump({"workflows": workflows})
     return contents
 
 
-def lint_workflow_artifacts_on_paths(ctx, paths, lint_args):
+def lint_workflow_artifacts_on_paths(
+    ctx: "PlanemoCliContext", paths: Iterable[str], lint_args: Dict[str, Union[str, List[str]]]
+) -> int:
     report_level = lint_args["level"]
     lint_context = WorkflowLintContext(report_level, skip_types=lint_args["skip_types"])
     for path in paths:
@@ -70,7 +86,9 @@ def lint_workflow_artifacts_on_paths(ctx, paths, lint_args):
         return EXIT_CODE_OK
 
 
-def _lint_workflow_artifacts_on_path(lint_context, path, lint_args):
+def _lint_workflow_artifacts_on_path(
+    lint_context: WorkflowLintContext, path: str, lint_args: Dict[str, Union[str, List[str]]]
+) -> None:
     for potential_workflow_artifact_path in find_potential_workflow_files(path):
         if os.path.basename(potential_workflow_artifact_path) == DOCKSTORE_REGISTRY_CONF:
             lint_context.lint("lint_dockstore", _lint_dockstore_config, potential_workflow_artifact_path)
@@ -93,7 +111,7 @@ def _lint_workflow_artifacts_on_path(lint_context, path, lint_args):
 
 
 # misspell for pytest
-def _lint_tsts(path, lint_context):
+def _lint_tsts(path: str, lint_context: WorkflowLintContext) -> None:
     runnables = for_path(path, return_all=True)
     if not isinstance(runnables, list):
         runnables = [runnables]
@@ -112,7 +130,7 @@ def _lint_tsts(path, lint_context):
             lint_context.valid(f"Tests appear structurally correct for {runnable.path}")
 
 
-def _lint_best_practices(path, lint_context):  # noqa: C901
+def _lint_best_practices(path: str, lint_context: WorkflowLintContext) -> None:  # noqa: C901
     """
     This function duplicates the checks made by Galaxy's best practices panel:
     https://github.com/galaxyproject/galaxy/blob/5396bb15fe8cfcf2e89d46c1d061c49b60e2f0b1/client/src/components/Workflow/Editor/Lint.vue
@@ -181,7 +199,7 @@ def _lint_best_practices(path, lint_context):  # noqa: C901
         # unlabeled outputs are checked by gxformat2, no need to check here
 
 
-def _lint_case(path, test_case, lint_context):
+def _lint_case(path: str, test_case: TestCase, lint_context: WorkflowLintContext) -> bool:
     test_valid = True
 
     i_labels = input_labels(workflow_path=path)
@@ -228,17 +246,19 @@ def _lint_case(path, test_case, lint_context):
     return test_valid
 
 
-def _check_test_assertions(lint_context, assertion_definitions):
+def _check_test_assertions(
+    lint_context: WorkflowLintContext, assertion_definitions: Optional[Dict[str, Dict[str, Any]]]
+) -> bool:
     # we are already in Python, not XML, so it is simpler to lint assertions by checking against the
     # Python functions directly, rather than checking against galaxy.xsd as for tool linting
     assertions_valid = True
     if assertion_definitions:
-        for assertion_name in assertion_definitions:
+        for assertion_name, assertion_params in assertion_definitions.items():
             function = asserts.assertion_functions[f"assert_{assertion_name}"]
             signature = inspect.signature(function)
             try:
                 # try mapping the function with the attributes supplied and check for TypeError
-                signature.bind("", **assertion_definitions[assertion_name])
+                signature.bind("", **assertion_params)
             except AssertionError:
                 pass
             except TypeError as e:
@@ -247,7 +267,12 @@ def _check_test_assertions(lint_context, assertion_definitions):
     return assertions_valid
 
 
-def _tst_input_valid(test_case, input_id, input_def, lint_context):
+def _tst_input_valid(
+    test_case: TestCase,
+    input_id: str,
+    input_def: Dict[str, Any],
+    lint_context: WorkflowLintContext,
+) -> bool:
     if type(input_def) == dict:  # else assume it is a parameter
         clazz = input_def.get("class")
         if clazz == "File":
@@ -267,7 +292,7 @@ def _tst_input_valid(test_case, input_id, input_def, lint_context):
     return True
 
 
-def _lint_dockstore_config(path, lint_context):
+def _lint_dockstore_config(path: str, lint_context: WorkflowLintContext) -> None:
     dockstore_yaml = None
     try:
         with open(path) as f:
@@ -293,7 +318,9 @@ def _lint_dockstore_config(path, lint_context):
         _lint_dockstore_workflow_entry(lint_context, os.path.dirname(path), workflow_entry)
 
 
-def _lint_dockstore_workflow_entry(lint_context, directory, workflow_entry):
+def _lint_dockstore_workflow_entry(
+    lint_context: WorkflowLintContext, directory: str, workflow_entry: Dict[str, Any]
+) -> None:
     if not isinstance(workflow_entry, dict):
         lint_context.error("Invalid YAML contents found in %s, workflow entry not a dict" % DOCKSTORE_REGISTRY_CONF)
         return
@@ -322,7 +349,7 @@ def _lint_dockstore_workflow_entry(lint_context, directory, workflow_entry):
             lint_context.error(f"{DOCKSTORE_REGISTRY_CONF} workflow entry references absent file {referenced_file}")
 
 
-def looks_like_a_workflow(path):
+def looks_like_a_workflow(path: str) -> bool:
     """Return boolean indicating if this path looks like a workflow."""
     if POTENTIAL_WORKFLOW_FILES.match(os.path.basename(path)):
         with open(path) as f:
@@ -330,17 +357,17 @@ def looks_like_a_workflow(path):
         if not isinstance(workflow_dict, dict):
             # Not exactly right - could have a #main def - do better and sync with Galaxy.
             return False
-        return workflow_dict.get("class") == "GalaxyWorkflow" or workflow_dict.get("a_galaxy_workflow")
+        return bool(workflow_dict.get("class") == "GalaxyWorkflow" or workflow_dict.get("a_galaxy_workflow"))
     return False
 
 
-def find_workflow_descriptions(directory):
+def find_workflow_descriptions(directory: str) -> Iterator[str]:
     for potential_workflow_artifact_path in find_potential_workflow_files(directory):
         if looks_like_a_workflow(potential_workflow_artifact_path):
             yield potential_workflow_artifact_path
 
 
-def find_potential_workflow_files(directory):
+def find_potential_workflow_files(directory: str) -> List[str]:
     """Return a list of potential workflow files in a directory."""
     if not os.path.exists(directory):
         raise ValueError(f"Directory not found {directory}")
