42a3e36d2640cc2135079cbce6acfc32bba458a4
Marius van den Beek <m.vandenbeek@gmail.com>
2022-11-02 16:23:34 +0100

    Use for_paths directly
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>

--- planemo/runnable.py
+++ planemo/runnable.py
@@ -189,14 +189,7 @@ def workflow_dir_runnables(path: str) -> List[Runnable]:
 
 
 def tool_dir_runnables(path: str) -> List[Runnable]:
-    runnables = []
-    for tool_path, _ in yield_tool_sources_on_paths(ctx=None, paths=[path]):
-        runnable = for_path(tool_path)
-        if isinstance(runnable, list):
-            runnables.extend(runnable)
-        else:
-            runnables.append(runnable)
-    return runnables
+    return for_paths(tool_path for tool_path, _ in yield_tool_sources_on_paths(ctx=None, paths=[path]))
 
 
 def for_path(path: str) -> Union[Runnable, List[Runnable]]:
