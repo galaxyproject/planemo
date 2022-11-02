810f7bdcd77d944153e3202817b716e1b54659b2
Lucille Delisle <lucille.delisle@epfl.ch>
2022-11-03 00:20:52 +0100

    run black

--- tests/test_cmd_workflow_lint.py
+++ tests/test_cmd_workflow_lint.py
@@ -225,5 +225,6 @@ class CmdWorkflowLintTestCase(CliTestCase):
         lint_cmd = ["workflow_lint", workflow_path]
         self._check_exit_code(lint_cmd, exit_code=0)
 
+
 def _wf_repo(rel_path):
     return os.path.join(TEST_DATA_DIR, "wf_repos", rel_path)
