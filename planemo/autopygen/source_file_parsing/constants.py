3f068014d811725a6f0b5a313460b0770a289c70
Matthias Bernt <m.bernt@ufz.de>
2023-08-28 19:42:11 +0200

    one more try

--- planemo/autopygen/source_file_parsing/constants.py
+++ planemo/autopygen/source_file_parsing/constants.py
@@ -2,8 +2,8 @@
 try:
     from sys import stdlib_module_names
 except ImportError:
-    from stdlib_list import stdlib_list as stdlib_module_names
-
+    from stdlib_list import stdlib_list
+    stdlib_module_names = stdlib_list()
 WARNING_STRING = "##!_FIXME_!##"
 
 STD_LIB_MODULE_NAMES = frozenset(stdlib_module_names)
