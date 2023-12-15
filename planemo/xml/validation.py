901b2895723a837a804bc5ab025bd50928602f8b
Matthias Bernt <m.bernt@ufz.de>
2023-12-15 16:34:14 +0100

    Revert "remove lxml based schema linter"
    This reverts commit bc4a3f25f4801f4a931e3ff14d0b6b0626feede4.
    
    The xsd linting is used also to other xml files,
    tool_dependencies.xml. So I guess the changed default for
    `--xsd` should be suffiecient

--- planemo/xml/validation.py
+++ planemo/xml/validation.py
@@ -4,6 +4,7 @@ import abc
 import subprocess
 from collections import namedtuple
 
+from galaxy.util import unicodify
 from galaxy.util.commands import which
 
 try:
@@ -14,7 +15,7 @@ except ImportError:
 XMLLINT_COMMAND = "xmllint --noout --schema {0} {1} 2>&1"
 INSTALL_VALIDATOR_MESSAGE = (
     "This feature requires an external dependency "
-    "to function, please install xmllint (e.g 'brew "
+    "to function, pleaes install xmllint (e.g 'brew "
     "install libxml2' or 'apt-get install "
     "libxml2-utils'."
 )
@@ -41,11 +42,27 @@ class XsdValidator(metaclass=abc.ABCMeta):
 ValidationResult = namedtuple("ValidationResult", ["passed", "output"])
 
 
+class LxmlValidator(XsdValidator):
+    """Validate XSD files using lxml library."""
+
+    def validate(self, schema_path, target_path):
+        try:
+            xsd_doc = etree.parse(schema_path)
+            xsd = etree.XMLSchema(xsd_doc)
+            xml = etree.parse(target_path)
+            passed = xsd.validate(xml)
+            return ValidationResult(passed, xsd.error_log)
+        except etree.XMLSyntaxError as e:
+            return ValidationResult(False, unicodify(e))
+
+    def enabled(self):
+        return etree is not None
+
+
 class XmllintValidator(XsdValidator):
     """Validate XSD files with the external tool xmllint."""
 
     def validate(self, schema_path, target_path):
-        print("XmllintValidator")
         command = XMLLINT_COMMAND.format(schema_path, target_path)
         p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
         stdout, _ = p.communicate()
@@ -56,7 +73,7 @@ class XmllintValidator(XsdValidator):
         return bool(which("xmllint"))
 
 
-VALIDATORS = [XmllintValidator()]
+VALIDATORS = [LxmlValidator(), XmllintValidator()]
 
 
 def get_validator(require=True):
