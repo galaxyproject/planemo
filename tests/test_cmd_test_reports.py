1f7a366f717ac3185a704144641cacabd84e38b3
mvdbeek <m.vandenbeek@gmail.com>
2024-07-01 17:42:43 +0200

    Fix up the test

--- tests/test_cmd_test_reports.py
+++ tests/test_cmd_test_reports.py
@@ -24,17 +24,13 @@ class CmdTestReportsTestCase(CliTestCase):
     def test_markdown(self):
         with self._isolate() as f:
             json_path = os.path.join(TEST_DATA_DIR, "issue381.json")
-            results_path = os.path.join(f, "minimal_markdown_results")
-            self._check_exit_code(["test_reports", "--test_output_minimal_markdown", results_path, json_path], exit_code=0)
+            results_path = os.path.join(f, "markdown_results")
+            self._check_exit_code(["test_reports", "--test_output_markdown", results_path, json_path], exit_code=0)
             assert os.path.exists(results_path)
-            assert os.path.isdir(results_path)
-            assert len(os.listdir(results_path))
 
+            # Run minimal version
             minimal_results_path = os.path.join(f, "minimal_markdown_results")
-            self._check_exit_code(["test_reports", "--test_output_minimal_markdown", minimal_results_path, json_path], exit_code=0)
+            self._check_exit_code(["test_reports", "--test_output_markdown_minimal", minimal_results_path, json_path], exit_code=0)
             assert os.path.exists(minimal_results_path)
-            assert os.path.isdir(minimal_results_path)
-            assert len(os.listdir(minimal_results_path))
-
-            minimal_markdown_results = os.path.join(minimal_results_path, "tool_test_output.md")
-            assert os.path.getsize(minimal_markdown_results) < os.path.getsize(markdown_results)
+            # Make sure minimal markdown is compacted
+            assert os.path.getsize(minimal_results_path) < os.path.getsize(results_path)
