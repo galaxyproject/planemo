031e7d31aeead864296bee3ad623c5bb2285eb62
Leonid Kostrykin <void@evoid.de>
2025-12-15 22:03:14 +0100

    Add `"requirements_in_conda"` and `"biocontainer_registered"` to `linters`

--- planemo/lint.py
+++ planemo/lint.py
@@ -46,7 +46,7 @@ def build_lint_args(ctx: "PlanemoCliContext", **kwds) -> Dict[str, Any]:
                 skip_types.append(line)
 
     linters = Linter.list_linters()
-    linters.extend(["version_bumped"])
+    linters.extend(["version_bumped", "requirements_in_conda", "biocontainer_registered"])
     invalid_skip_types = list(set(skip_types) - set(linters))
     if len(invalid_skip_types):
         error(f"Unknown linter type(s) {invalid_skip_types} in list of linters to be skipped. Known linters {linters}")
